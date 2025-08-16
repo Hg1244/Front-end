29d76c5818b331122213f9eae949ee7fbfb8131a
Hg1244 <144611205+Hg1244@users.noreply.github.com>
2025-08-16 08:54:30 +0530

    Add files via upload

---
+++ UTBE.js
@@ -0,0 +1,62 @@
+let b=10;
+console.log(b);
+   
+var price =100;
+var product="box";
+var tax=2000;
+console.log(product);
+var total=price+tax;
+console.log(total);
+
+var fruitname="apple"
+var count=2;
+var price=100
+var total=count*price;
+console.log(fruitname);
+console.log(total);
+
+let c=10;
+d=c--;
+e=d--
+console.log(c);
+console.log(d);
+console.log(e);
+
+function add(g,h){
+    console.log(g+h);
+}
+add(2,3);
+
+var factor="Kamal";
+var fplayer="Dhoni";
+var fmovie="Anbe sivam";
+
+function favourite()
+{
+   console.log("Factor is:"+factor);
+    console.log("fplayer is:"+fplayer);
+    console.log("fmovie is:"+fmovie);
+
+}
+favourite();
+
+
+
+function area(length,breadth)
+{
+    console.log(length*breadth)
+}
+area(10,20);
+
+
+function perimeter(a,b,c)
+{
+    console.log(a+b+c);
+}
+ perimeter(10,34,45);
+ 
+function add1(a1,b1)
+{
+   return a1+b1;
+}
+console.log(add1(10,20));
